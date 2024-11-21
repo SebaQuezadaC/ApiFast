fcae716a11c80ff66913c696f7f7345256dd0416
Sebastian Quezada <s.quezadac@outlook.com>
2024-11-21 05:59:36 -0400

    primer commit

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = "postgresql://SEBA:2004@localhost:5432/TEST"
+DATABASE_URL = "postgresql://"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
